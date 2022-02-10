510a201131638bfc32cc227b52b1eb61935931f6
giuseppesiciliano <giuseppesiciliano90@gmail.com>
2022-02-10 15:04:47 +0100

    thumb preview single magazine

--- resources/views/magazine.blade.php
+++ resources/views/magazine.blade.php
@@ -6,5 +6,23 @@
 
 
 @section('main_content')
-    ciao
+
+    <div class="banner-blue">
+        <div class="container">
+
+            {{-- Thumb --}}
+            <div class="thumb-magazine">
+                <img src="{{ $magazine['thumb'] }}" alt="">
+                <h5>VIEW GALLERY</h5>
+            </div>
+        </div>
+    </div>
+
+    <div class="info-magazine">
+        <div class="details">
+
+            <h1 class="title">{{ $magazine['title'] }}</h1>
+        </div>
+    </div>
+
 @endsection
